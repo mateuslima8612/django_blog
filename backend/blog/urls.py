eda50dc4bd5851b04afcf3f0536d9aee019faec0
Mateus de Lima <mateusrodrigues8612@gmail.com>
2025-08-19 21:44:10 -0300

    about page

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('posts/id/<int:pk>/', views.post_detail, name='post-detail'),
     path('posts/', views.posts, name='posts'),
     path('posts/<int:page>', views.posts, name='posts-paginated'),
+    path('about', views.about, name='about')
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -22,3 +22,6 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
 
+def about(request):
+    return render(request, 'about.html')
+
