4cbd16badab8f3b0529b633af13186b2b777b76f
Mateus de Lima <mateusrodrigues8612@gmail.com>
2025-11-25 17:51:14 -0300

    .

---
+++ vercel_app.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+# Add the backend directory to the Python path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'backend'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+application = get_wsgi_application()
+app = application
