a6132ddbc2047dfd020abcd06386220c63fa0c3f
Mateus de Lima <mateusrodrigues8612@gmail.com>
2025-11-24 21:29:50 -0300

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+a@7n^=fmb!phzbqmi3u*hyo^sg=_en0w_5o&fhizu5k*a5_('
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
