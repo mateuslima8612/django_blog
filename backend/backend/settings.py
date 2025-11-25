ca7251e9fccd1093b0cf4d65cae251e8b578c109
Mateus de Lima <mateusrodrigues8612@gmail.com>
2025-11-25 00:25:52 -0300

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,7 +94,7 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': tmpPostgres.path.replace('/', ''),
+        'NAME': (tmpPostgres.path.decode() if isinstance(tmpPostgres.path, (bytes, bytearray)) else tmpPostgres.path).replace('/', ''),
         'USER': tmpPostgres.username,
         'PASSWORD': tmpPostgres.password,
         'HOST': tmpPostgres.hostname,
